bfd2dd93f3cd08ba0d196427e482457160fd86db
Anna <ann1989@list.ru>
2022-07-15 00:24:08 +0300

    Homework :wq

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -5,8 +5,8 @@
 // 918 -> 1
 
 Console.WriteLine("Введите 3-х значное число: ");
-int n=Convert.ToInt32(Console.ReadLine());
-int result = (n % 100)/10;
+int n = Convert.ToInt32(Console.ReadLine());
+int result = (n % 100) / 10;
 if (n >= 100 && n < 1000)
     Console.WriteLine(result);
 else
